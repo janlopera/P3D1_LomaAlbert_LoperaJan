16206697ba614a4ca724d698655cf76367895589
Jan Lopera <jlopera@edu.tacnocampus.cat>
2020-10-05 11:53:02 +0200

    Added WIP Controllers

--- Assets/Scripts/Controllers/FPSController.cs
+++ Assets/Scripts/Controllers/FPSController.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
+
+    private MovementController _movementController;
+    private ViewController _viewController;
+    private CharacterController _characterController;
+
+
+    [SerializeField]
+    private GameObject _cameraObject;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        
+        
+        _movementController = GetComponent<MovementController>();
+        _movementController.Constructor(_characterController);
+        _viewController = _cameraObject.GetComponent<ViewController>();
+    }
+
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _movementController.OnUpdate();
+        _viewController.OnUpdate();
     }
 }
