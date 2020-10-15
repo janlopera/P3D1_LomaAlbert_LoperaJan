1aba1a84b3743efddbe7c5ce9e6067be63b95793
Jan Lopera <jlopera@edu.tacnocampus.cat>
2020-10-15 12:33:51 +0200

    Added Scriptable Object

---
+++ Assets/Scripts/Models/Weapons/WeaponStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : ScriptableObject
+{
+    public float Price;
+    public float Damage;
+    public float RangeModifier;
+    public float CycleTime;
+    public float Penetration;
+    public float KillAward;
+    public float MaxPlayerSpeed;
+    public int ClipSize;
+    public float Range;
+    public bool IsFullAuto;
+    public int BulletsPerShot;
+    public int ReserveAmo;
+    
+    public int ResetTime;
+    public int ReloadTime;
+}
