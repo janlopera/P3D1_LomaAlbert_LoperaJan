225a8af46e1e986bfa5059dd581eb059ff3e3fff
albert loma <aloma@edu.tecnocampus.cat>
2020-10-22 19:26:24 +0200

    tenenmos final

---
+++ Assets/Scripts/Controllers/EndgameController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class EndgameController : MonoBehaviour
+{
+
+
+    public List<GameObject> enemiesAlive;
+    public int delayTime;
+    public int timeBetweenExplosions;
+    public List<GameObject> Explosions;
+
+    private void Start()
+    {
+        foreach (GameObject g in Explosions)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (enemiesAlive[0] == null && enemiesAlive[1] == null)
+        {
+            StartFinalScene();
+        }
+    }
+
+    async Task StartFinalScene()
+    {
+
+        await Task.Delay(delayTime);
+        foreach (GameObject g in Explosions)
+        {
+            g.SetActive(true);
+        }
+
+    }
+}
