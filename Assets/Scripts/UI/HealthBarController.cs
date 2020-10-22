190e2f97fa08e1591e57b0ffd9f579f85d5648bc
albert loma <aloma@edu.tecnocampus.cat>
2020-10-22 20:36:39 +0200

    un regalo brouston

---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Behaviours;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public HealthSystem _healthSystem;
+    private Slider _healthbarindicator;
+    private float lastHealth;
+    void Start()
+    {
+        _healthbarindicator = this.GetComponent<Slider>();
+        lastHealth = _healthSystem.Health;
+        _healthbarindicator.maxValue = _healthSystem.MAX_HEALTH;
+        _healthbarindicator.value = _healthbarindicator.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastHealth != _healthSystem.Health)
+        {
+            _healthbarindicator.value = _healthSystem.Health;
+        }
+    }
+}
